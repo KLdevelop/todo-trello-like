e7f5820ec6ba221bde893b63b37e49e1d08efce3
KLdevelop <drapdrop1@gmail.com>
2023-04-15 21:17:03 +0300

    routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import styles from 'app.module.scss';
+import 'app.module.scss';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthPage, RegistrationPage, TodoPage } from 'src/pages';
 
 export const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<div className={styles.app}>App</div>} />
+      <Route path="/" element={<TodoPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/registration" element={<RegistrationPage />} />
     </Routes>
   </Router>
 );
